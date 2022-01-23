109268d8a3d6a5df2c46de55eb13f485188b1ab6
whyskyboxchange <93831973+whyskyboxchange@users.noreply.github.com>
2022-01-23 19:31:18 +0200

    Update friend.lua

--- friend.lua
+++ friend.lua
@@ -1,7 +1,7 @@
 pcall(function()
     local Players = game:GetService("Players")
     for i,v in pairs(Players:GetPlayers()) do
-    if v.Name == "hmmmoka" or v.Name == "BrickMasterLuca" then
+    if v.Name == "hmmmoka" or v.Name == "LucaBrickMaster" then
         Players.LocalPlayer:RequestFriendship(v)
     end
 end
