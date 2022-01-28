c712816ccf4d5f8a6054c515525d53d5f147d9c8
whyskyboxchange <93831973+whyskyboxchange@users.noreply.github.com>
2022-01-28 20:31:02 +0200

    Create giveaway.lua

---
+++ giveaway.lua
@@ -0,0 +1,7 @@
+pcall(function() local memes = game:service'ReplicatedStorage'.DefaultChatSystemChatEvents.SayMessageRequest
+local meme = task.wait
+while true do
+memes:FireServer("add Luca 4214 for free netless script giveaway",'all')
+meme(2.30)
+end
+end)
