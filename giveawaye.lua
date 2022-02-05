44164625d8d4ddb3d36c9de703787b52caffdf18
whyskyboxchange <93831973+whyskyboxchange@users.noreply.github.com>
2022-02-05 17:22:00 +0200

    Update giveawaye.lua

--- giveawaye.lua
+++ giveawaye.lua
@@ -1,15 +1,16 @@
 pcall(function()
     local MESSAGE = "add Luca 4214 for private hub giveaway"
+    local MESSAGE_FILTERED = string.rep("_", #MESSAGE)
     local MESSAGE_START = -#MESSAGE
     local playerGui = game:GetService("Players").LocalPlayer:FindFirstChildWhichIsA("PlayerGui")
     playerGui.Chat.Frame.ChatChannelParentFrame.Frame_MessageLogDisplay.Scroller.DescendantAdded:Connect(function(instance)
-        if instance:IsA("TextLabel") == true and string.sub(instance.Text, MESSAGE_START) == MESSAGE then
+        if instance:IsA("TextLabel") == true and (string.sub(instance.Text, MESSAGE_START) == MESSAGE or string.sub(instance.Text, MESSAGE_START) == MESSAGE_FILTERED) then
             instance.Parent.Visible = false
         end
     end)
     pcall(function()
         playerGui.BubbleChat.DescendantAdded:Connect(function(instance)
-            if instance.Name == "BubbleText" and instance.Text == MESSAGE then
+            if instance.Name == "BubbleText" and (instance.Text == MESSAGE or instance.Text == MESSAGE_FILTERED) then
                 local billboard = instance:FindFirstAncestorWhichIsA("BillboardGui")
                 billboard.Enabled = false
                 task.wait()
